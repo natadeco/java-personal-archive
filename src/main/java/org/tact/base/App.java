720d9fa80ada2e25f55626b18e959a6c92899ad4
natadeco <alikings97.sa@gmail.com>
2019-02-19 23:08:07 +0530

    FileReader Module is Added

--- src/main/java/org/tact/base/App.java
+++ src/main/java/org/tact/base/App.java
@@ -1,12 +1,25 @@
 package org.tact.base;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class App {
 
-	public static void main(String[] args) {
-		System.out.println(App.add(14,17));
+	public static void main(String[] args) throws IOException {
+		@SuppressWarnings("resource")
+		Scanner objj = new Scanner(System.in);
+		System.out.println("Enter two integers to add");
+		int a = objj.nextInt();
+		int b = objj.nextInt();
+		System.out.println("Addition of given integer is " + App.add(a,b));
+		System.out.println("\n");
+		System.out.println("Enter the file path");
+		String folder = objj.next();
+		Filereader.file(folder);
 	}
 	
 	public static int add(int a, int b) {
 		return a+b;
 	}
+	
 }
---
+++ src/main/java/org/tact/base/Filereader.java
@@ -0,0 +1,46 @@
+package org.tact.base;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Filereader  {
+	
+	public static void file(String folder) throws FileNotFoundException {
+	File file1 = new File(folder);
+	Filereader  fi = new Filereader();
+	fi.listFiles(file1);
+	}
+
+	private void listFiles(File file) {
+		// TODO Auto-generated method stub
+		System.out.println("Reading the files from the folder");
+		File[] fileNames = file.listFiles();
+		for(File file2 : fileNames) {
+			if(file2.isDirectory()) {
+			  listFiles(file2);
+			}else {
+				try {
+					readContent(file2);
+				}catch(IOException e){
+					e.printStackTrace();
+				}
+			}
+		}
+		
+	}
+
+	private void readContent(File file2) throws IOException {
+		// TODO Auto-generated method stub
+		try(BufferedReader br  = new BufferedReader(new FileReader(file2))){
+            String strLine;
+            while((strLine = br.readLine()) != null){
+             System.out.println("Line is - " + strLine);
+            }
+		}
+	}
+	
+	
+}
