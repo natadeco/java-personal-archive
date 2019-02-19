f4ed74ede43a1a1194629b94f32dabe45c192240
natadeco <alikings97.sa@gmail.com>
2019-02-19 21:15:58 +0530

    Add Method wiht return statement updated

--- src/main/java/org/tact/base/App.java
+++ src/main/java/org/tact/base/App.java
@@ -3,6 +3,10 @@ package org.tact.base;
 public class App {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Java Personal Archive");
+		System.out.println(App.add(14,17));
+	}
+	
+	public static int add(int a, int b) {
+		return a+b;
 	}
 }
